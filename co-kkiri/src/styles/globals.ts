9a610e8acb71dee11f8d7c6655b1a7e145108b5c
김주현 <trust987@naver.com>
2024-03-06 12:18:56 +0900

    Chore: 디자인 토큰 셋팅

--- co-kkiri/src/styles/globals.ts
+++ co-kkiri/src/styles/globals.ts
@@ -13,6 +13,35 @@ body {
   font-family: "Pretendard";
   font-weight: 400;
 }
+
+@font-face {
+    font-family: Pretendard;
+    font-weight: 400;
+    font-display: swap;
+    src: url("../../public/fonts/Pretendard-Regular.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: Pretendard;
+    font-weight: 500;
+    font-display: swap;
+    src: url("../../public/fonts/Pretendard-Medium.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: Pretendard;
+    font-weight: 600;
+    font-display: swap;
+    src: url("../../public/fonts/Pretendard-SemiBold.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: Pretendard;
+    font-weight: 700;
+    font-display: swap;
+    src: url("../../public/fonts/Pretendard-Bold.woff") format("woff");
+  }
+
 `;
 
 export default GlobalStyles;
