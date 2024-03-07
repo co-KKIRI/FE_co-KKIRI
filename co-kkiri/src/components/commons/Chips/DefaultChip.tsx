d2d35d36bcf3f3f448093b3e9c063d18e0e9fe93
nakyoung98 <jay3925@cau.ac.kr>
2024-03-07 11:08:34 +0900

    Feat: DefaultChip 컴포넌트 및 Container 스타일 구현

---
+++ co-kkiri/src/components/commons/Chips/DefaultChip.tsx
@@ -0,0 +1,40 @@
+import { MouseEvent } from "react";
+import DESIGN_TOKEN from "@/styles/tokens";
+import styled, { CSSProperties } from "styled-components";
+
+interface DefaultChipProps {
+  label: string;
+  isSelected?: boolean;
+  onClick?: (e: MouseEvent<HTMLDivElement>) => void;
+  style?: DefaultChipContainerStyleProps;
+  selectedStyle?: DefaultChipContainerStyleProps;
+}
+
+export default function DefaultChip({ label, isSelected, onClick, style, selectedStyle }: DefaultChipProps) {
+  const currentStyle = isSelected ? selectedStyle : style;
+  return (
+    <DefaultChipContainer {...currentStyle} onClick={onClick}>
+      {label}
+    </DefaultChipContainer>
+  );
+}
+
+interface DefaultChipContainerStyleProps {
+  $padding?: CSSProperties["padding"];
+  $backgroundColor?: CSSProperties["backgroundColor"];
+  $fontColor?: CSSProperties["color"];
+  $borderRadius?: CSSProperties["borderRadius"];
+}
+
+const { color, typography } = DESIGN_TOKEN;
+
+const DefaultChipContainer = styled.div<DefaultChipContainerStyleProps>`
+  width: fit-content;
+  padding: ${({ $padding }) => $padding || `0.4rem 1.2rem`};
+
+  background-color: ${({ $backgroundColor }) => $backgroundColor || color.gray[300]};
+  color: ${({ $fontColor }) => $fontColor || color.black[300]};
+  border-radius: ${({ $borderRadius }) => $borderRadius || `9999rem`};
+
+  ${typography.font12Semibold}
+`;
