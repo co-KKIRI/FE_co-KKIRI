bd0ea09e9791d9456612f90fbceddc2c05423d6d
Yoon-kyungLee <yk2588@nate.com>
2024-03-10 15:53:19 +0900

    Chore: 모달 테스트 코드 파일 작성

---
+++ co-kkiri/src/components/modals/UserProfileModal/test.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import UserProfileModal from "@/components/modals/UserProfileModal/";
+
+export default function ModalTestPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const userInfo = {
+    profileImg: "",
+    position: ["프론트엔드", "백엔드"],
+    nickname: "123",
+    career: 2,
+    stack: [
+      { id: 1, img: "https://simpleicons.org/icons/react.svg" },
+      { id: 2, img: "https://simpleicons.org/icons/nextdotjs.svg" },
+    ],
+    introduce: "잘 부탁드립니다.",
+    link: ["https://www.youtube.com"],
+  };
+
+  return (
+    <>
+      <button onClick={openModal}>Open Modal</button>
+      {isModalOpen && (
+        <UserProfileModal
+          profileImg={userInfo.profileImg}
+          position={userInfo.position}
+          nickname={userInfo.nickname}
+          career={userInfo.career}
+          stack={userInfo.stack}
+          introduce={userInfo.introduce}
+          link={userInfo.link}
+        />
+      )}
+    </>
+  );
+}
