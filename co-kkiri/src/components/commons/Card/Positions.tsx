459a65b1fe12872efeaa2a9e059f2e244d322ad6
Yoon-kyungLee <yk2588@nate.com>
2024-03-10 21:29:50 +0900

    Feat: position 값이 없을 경우 추가

--- co-kkiri/src/components/commons/Card/Positions.tsx
+++ co-kkiri/src/components/commons/Card/Positions.tsx
@@ -14,11 +14,15 @@ interface PositionsProps {
 export default function Positions({ position }: PositionsProps) {
   return (
     <Wrapper>
-      {position.map((item) => (
-        <div key={item.id}>
-          <PositionChip label={item.name} />
-        </div>
-      ))}
+      {position.length > 0 ? (
+        position.map((item) => (
+          <div key={item.id}>
+            <PositionChip label={item.name} />
+          </div>
+        ))
+      ) : (
+        <PositionChip label="포지션" />
+      )}
     </Wrapper>
   );
 }
