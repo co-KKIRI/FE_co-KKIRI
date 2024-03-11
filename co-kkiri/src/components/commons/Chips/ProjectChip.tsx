a5187fde2163a85776f145a17b8beef9a9a63967
이나경 <jay3925@cau.ac.kr>
2024-03-11 16:39:52 +0900

    Fix: ProjectChip 컴포넌트 변경된 DefaultChip에 맞춰 코드 수정

--- co-kkiri/src/components/commons/Chips/ProjectChip.tsx
+++ co-kkiri/src/components/commons/Chips/ProjectChip.tsx
@@ -1,27 +1,40 @@
-import DefaultChip, { DefaultChipContainerStyleProps } from "./DefaultChip";
+import styled, { css } from "styled-components";
+import DefaultChip from "./DefaultChip";
+import { VariantStyle } from "@/types/styledUtilTypes";
+
+export type ProjectType = "스터디" | "프로젝트";
 
 interface ProjectChipProps {
-  label: "스터디" | "프로젝트"; //나중에 global한 type으로 빼내어야함
+  label: ProjectType; //나중에 global한 type으로 빼내어야함
 }
-
-const ProjectChipStyle: DefaultChipContainerStyleProps = {
-  $padding: `.6rem 3rem`,
-  $borderRadius: `.2rem 6.1rem 6.1rem .2rem`,
-};
-
-const studyProjectChipStyle: DefaultChipContainerStyleProps = {
-  $backgroundColor: `#DAF4AF`,
-  $fontColor: `#588F00`,
-  ...ProjectChipStyle,
-};
-
-const projectProjectChipStyle: DefaultChipContainerStyleProps = {
-  $backgroundColor: `#FFDBE4`,
-  $fontColor: `#C71B44`,
-  ...ProjectChipStyle,
-};
 
 export default function ProjectChip({ label }: ProjectChipProps) {
-  const projectChipStyle = label === "스터디" ? studyProjectChipStyle : projectProjectChipStyle;
-  return <DefaultChip label={label} style={projectChipStyle} />;
+  return <Container label={label}/>;
 }
+
+
+type ProjectVariant = ProjectType;
+
+const ProjectChipStyle = css`
+  padding: .6rem 3rem;
+  border-radius: .2rem 6.1rem 6.1rem .2rem;
+  `
+  ;
+
+const VARIANT_STYLE: VariantStyle<ProjectVariant> = {
+  '스터디': css`
+    background-color: #DAF4AF;
+    color: #588F00;
+    ${ProjectChipStyle}
+    `
+  ,
+  '프로젝트': css`
+    background-color: #FFDBE4;
+    color:#C71B44;
+    ${ProjectChipStyle}
+  `
+}
+
+const Container = styled(DefaultChip)`
+  ${({ label }) => VARIANT_STYLE[label as ProjectVariant]}
+`
