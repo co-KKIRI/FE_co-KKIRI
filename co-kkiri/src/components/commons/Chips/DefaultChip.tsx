84353438ec9c775373712895b2cff21a41f90d14
이나경 <jay3925@cau.ac.kr>
2024-03-11 16:39:14 +0900

    Refactor: DefaultChip 컴포넌트 styled-component에 맞춰 전체 리팩토링

--- co-kkiri/src/components/commons/Chips/DefaultChip.tsx
+++ co-kkiri/src/components/commons/Chips/DefaultChip.tsx
@@ -1,44 +1,43 @@
 import { MouseEvent } from "react";
 import DESIGN_TOKEN from "@/styles/tokens";
-import styled, { CSSProperties } from "styled-components";
+import styled from "styled-components";
 
 interface DefaultChipProps {
   label: string;
   isSelected?: boolean;
   onClick?: (e: MouseEvent<HTMLDivElement>) => void;
-  style?: DefaultChipContainerStyleProps;
-  selectedStyle?: DefaultChipContainerStyleProps;
-  isVertical: boolean;
+  isVertical?: boolean;
+  className?: string;
 }
 
-export default function DefaultChip({ label, isSelected, onClick, style, selectedStyle, isVertical }: DefaultChipProps) {
-  const currentStyle = isSelected ? selectedStyle : style;
+export default function DefaultChip({ label, isSelected, onClick, isVertical, className }: DefaultChipProps) {
   return (
-    <Container $isVertical={isVertical} {...currentStyle} onClick={onClick}>
+    <Container className={className} $isVertical={isVertical} $isSelected={isSelected} onClick={onClick}>
       {label}
     </Container>
   );
 }
 
+/**
+ * @param $isVertical: boolean - 세로로 배치할지 여부
+ * @param $isSelected: boolean - 선택되었는지 여부
+ */
 export interface DefaultChipContainerStyleProps {
   $isVertical?: boolean;
-  $padding?: CSSProperties["padding"];
-  $backgroundColor?: CSSProperties["backgroundColor"];
-  $fontColor?: CSSProperties["color"];
-  $borderRadius?: CSSProperties["borderRadius"];
+  $isSelected?: boolean;
 }
 
 const { color, typography } = DESIGN_TOKEN;
 
 const Container = styled.div<DefaultChipContainerStyleProps>`
   width: fit-content;
-  padding: ${({ $padding }) => $padding || `0.4rem 1.2rem`};
+  padding: 0.4rem 1.2rem;
 
   display: flex;
 
-  background-color: ${({ $backgroundColor }) => $backgroundColor || color.gray[3]};
-  color: ${({ $fontColor }) => $fontColor || color.black[3]};
-  border-radius: ${({ $borderRadius }) => $borderRadius || `9999rem`};
+  background-color: ${color.gray[3]};
+  color: ${color.black[3]};
+  border-radius: 9999rem;
 
   ${typography.font12Semibold}
 
