1706a2c66937a29b74b96a86519c152413009a66
이나경 <jay3925@cau.ac.kr>
2024-03-12 02:48:55 +0900

    Feat: DefaultChip 이미지 및 icon 관련 코드 추가
    아이콘이 있을 경우 onClick이벤트가 Icon 클릭 시로 이동

--- co-kkiri/src/components/commons/Chips/DefaultChip.tsx
+++ co-kkiri/src/components/commons/Chips/DefaultChip.tsx
@@ -2,18 +2,29 @@ import { MouseEvent } from "react";
 import DESIGN_TOKEN from "@/styles/tokens";
 import styled from "styled-components";
 
+interface Icon {
+  src: string;
+  alt: string;
+}
+
 interface DefaultChipProps {
   label: string;
+  imgUrl?: string;
+  icon?: Icon;
+
   isSelected?: boolean;
   onClick?: (e: MouseEvent<HTMLDivElement>) => void;
+  onIconClick?: (e: MouseEvent<HTMLImageElement>) => void;
   isVertical?: boolean;
   className?: string;
 }
 
-export default function DefaultChip({ label, isSelected, onClick, isVertical, className }: DefaultChipProps) {
+export default function DefaultChip({ label, imgUrl, icon, isSelected, onClick, onIconClick, isVertical, className }: DefaultChipProps) {
   return (
-    <Container className={className} $isVertical={isVertical} $isSelected={isSelected} onClick={onClick}>
-      {label}
+    <Container className={className} $isVertical={isVertical} $isSelected={isSelected} $isClickable={(onClick || onIconClick) ? true : false} onClick={!icon ? onClick : undefined}>
+      {imgUrl && <div className="image-container"><img className="image" src={imgUrl} alt={label} /></div>}
+      <span className="label">{label}</span>
+      {icon && <img className="icon" src={icon.src} alt={icon.alt} onClick={onIconClick} />}
     </Container>
   );
 }
@@ -21,10 +32,12 @@ export default function DefaultChip({ label, isSelected, onClick, isVertical, cl
 /**
  * @param $isVertical: boolean - 세로로 배치할지 여부
  * @param $isSelected: boolean - 선택되었는지 여부
+ * @param $isClickable: boolean - 클릭 가능한지 여부
  */
 export interface DefaultChipContainerStyleProps {
   $isVertical?: boolean;
   $isSelected?: boolean;
+  $isClickable?: boolean
 }
 
 const { color, typography } = DESIGN_TOKEN;
@@ -44,11 +57,29 @@ const Container = styled.div<DefaultChipContainerStyleProps>`
   ${({ $isVertical }) => $isVertical ? `
       flex-direction: column;
       align-items: center;
-      gap: .4rem
+      gap: .4rem;
+      padding: .8rem;
     ` : `
       flex-direction: row;
       align-items: center;
-      gap: 1.2rem
+      gap: 1.2rem;
       `
-  })
+  }
+
+  ${({$isClickable}) => $isClickable && `cursor: pointer;`}
+
+  & .image-container{
+    width: 3.6rem;
+    height: 3.6rem;
+  }
+
+  & .image{
+    object-fit: cover;
+  }
+
+  & .icon{
+    width: 1.4rem;
+    height: 1.4rem;
+    ${({$isClickable}) => $isClickable && `cursor: pointer;`}
+  }
 `;
