6f4a37050314eef0b7b78307c8f30221e68ed3f8
unknown <llqrw55@naver.com>
2024-03-13 04:31:05 +0900

    Feat:Home페이지 복원

--- co-kkiri/src/components/modals/ModalLayout.tsx
+++ co-kkiri/src/components/modals/ModalLayout.tsx
@@ -30,13 +30,9 @@ export default function ModalLayout({
   useEffect(() => {
     document.body.style.cssText = `
       position: fixed; 
-      top: -${window.scrollY}px;
-      overflow-y: scroll;
       width: 100%;`;
     return () => {
-      const scrollY = document.body.style.top;
       document.body.style.cssText = "";
-      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
     };
   }, []);
 
