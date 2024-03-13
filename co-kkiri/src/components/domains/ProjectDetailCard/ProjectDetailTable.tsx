66ef3bc423999a2df475a6e97e57c95a247aa1d0
이나경 <jay3925@cau.ac.kr>
2024-03-13 11:14:18 +0900

    Feat: ProjectDetailTable 컴포넌트 완성

--- co-kkiri/src/components/domains/ProjectDetailCard/ProjectDetailTable.tsx
+++ co-kkiri/src/components/domains/ProjectDetailCard/ProjectDetailTable.tsx
@@ -1,3 +1,15 @@
-export default function ProjectDetailTable() {
-    return <div>ProjectDetailTable</div>;
+import styled from "styled-components";
+
+interface ProjectDetailTableProps {
+    children: React.ReactNode;
 }
+
+export default function ProjectDetailTable({children}: ProjectDetailTableProps) {
+    return <Container>{children}</Container>;
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
+`
