929028823b68921f5c1fc667669f8d4c7a3f962b
Haeun <han.kim0904@gmail.com>
2024-03-15 18:58:57 +0900

    Feat: OneLinerInput 컴포넌트 구현

---
+++ co-kkiri/src/components/commons/OneLinerInput.tsx
@@ -0,0 +1,58 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+import { styled } from "styled-components";
+import DESIGN_TOKEN from "@/styles/tokens";
+
+interface OneLinerInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+const OneLinerInput = forwardRef<HTMLInputElement, OneLinerInputProps>(
+  ({ label, id, placeholder, value, maxLength, onChange, onBlur }, ref) => {
+    return (
+      <Wrapper>
+        <Label htmlFor={id}>{label}</Label>
+        <Input
+          id={id}
+          placeholder={placeholder}
+          value={value}
+          maxLength={maxLength}
+          onChange={onChange}
+          onBlur={onBlur}
+          ref={ref}
+        />
+      </Wrapper>
+    );
+  },
+);
+
+export default OneLinerInput;
+
+const {
+  color,
+  typography: { font16Bold, font16Medium },
+} = DESIGN_TOKEN;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+
+const Label = styled.label`
+  color: ${color.black[1]};
+  ${font16Bold};
+`;
+
+const Input = styled.input`
+  ${font16Medium};
+  background: ${color.white};
+  border: 1px solid ${color.gray[2]};
+  border-radius: 0.5rem;
+  color: ${color.black[1]};
+  padding: 1.4rem 2rem 1.5rem;
+  width: 100%;
+
+  &:focus {
+    outline: none;
+  }
+`;
