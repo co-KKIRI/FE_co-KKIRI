4a5dd14026dcc9ec64276d5c69bec9e753797eb2
김주현 <trust987@naver.com>
2024-03-16 03:16:35 +0900

    Chore: BackButton, ShareButton 버튼 사이즈 수정

--- co-kkiri/src/components/domains/detail/ShareButton.tsx
+++ co-kkiri/src/components/domains/detail/ShareButton.tsx
@@ -1,4 +1,5 @@
 import { ICONS } from "@/constants/icons";
+import styled from "styled-components";
 
 export default function ShareButton() {
   const handleShare = () => {};
@@ -6,8 +7,21 @@ export default function ShareButton() {
   const { share } = ICONS;
 
   return (
-    <button onClick={handleShare}>
-      <img src={share.src} alt={share.alt} width={22} height={25} />
-    </button>
+    <Container onClick={handleShare}>
+      <Icon src={share.src} alt={share.alt} />
+    </Container>
   );
 }
+
+const Container = styled.button`
+  width: 4.2rem;
+  height: 4.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Icon = styled.img`
+  width: 2.2rem;
+  height: 2.5rem;
+`;
