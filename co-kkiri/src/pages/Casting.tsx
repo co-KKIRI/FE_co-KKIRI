84d1f482343d7b366bdf8fe6d0da73cb5c3d3048
Jisoo Han <llqrw55@naver.com>
2024-03-15 11:38:38 +0900

    Refactor:스카우트 페이지 복원

--- co-kkiri/src/pages/Casting.tsx
+++ co-kkiri/src/pages/Casting.tsx
@@ -1,11 +1,3 @@
-import RecruitDropdownButton from "@/components/commons/SelectDropButtons/RecruitDropdownButton";
-import UserInfoDropdownButton from "@/components/commons/SelectDropButtons/UserInfoDropdownButton";
-
 export default function Casting() {
-  return (
-    <div>
-      <UserInfoDropdownButton userInfoType="position" />
-      <RecruitDropdownButton recruitInfoType="meeting" />
-    </div>
-  );
+  return <div>Casting</div>;
 }
