e4bb33b7af716bab92360016cd6e89bf747ce1bd
이나경 <jay3925@cau.ac.kr>
2024-03-16 03:26:24 +0900

    Chore: 코드 여백 수정

--- co-kkiri/src/components/commons/FilterList.tsx
+++ co-kkiri/src/components/commons/FilterList.tsx
@@ -3,58 +3,58 @@ import { MouseEvent, useState } from "react";
 import styled from "styled-components";
 
 interface FilterListProps {
-    filters: string[];
-    onFilterClick(filter: string): void;
+  filters: string[];
+  onFilterClick(filter: string): void;
 }
 
 export default function FilterList({ filters, onFilterClick }: FilterListProps) {
-    const [currentFilter, setCurrentFilter] = useState<string>(filters[0]);
+  const [currentFilter, setCurrentFilter] = useState<string>(filters[0]);
 
-    const handleFilterClick = (e: MouseEvent<HTMLDivElement>) => {
-        const filter = e.currentTarget.textContent;
-        if (filter) {
-            console.log(filter + " clicked");
-            onFilterClick(filter);
-            setCurrentFilter(filter);
-        }
+  const handleFilterClick = (e: MouseEvent<HTMLDivElement>) => {
+    const filter = e.currentTarget.textContent;
+    if (filter) {
+      onFilterClick(filter);
+      setCurrentFilter(filter);
     }
+  };
 
-    return <Container>
-        {filters.map(filter => <Box key={filter} $isSelected={currentFilter === filter} onClick={handleFilterClick}>{filter}</Box>)}
+  return (
+    <Container>
+      {filters.map((filter) => (
+        <Box key={filter} $isSelected={currentFilter === filter} onClick={handleFilterClick}>
+          {filter}
+        </Box>
+      ))}
     </Container>
-
+  );
 }
 
 const { color, typography, mediaQueries } = DESIGN_TOKEN;
 
 const Container = styled.div`
-    display: flex;
+  display: flex;
+  gap: 2rem;
 
-    color: ${color.gray[1]};
+  color: ${color.gray[1]};
 
-    ${mediaQueries.mobile}{
-        gap: 1.6rem;
+  ${typography.font16Bold}
 
-        ${typography.font14Bold}
-    }
-    ${mediaQueries.tablet}{
-        gap: 2rem;
+  ${mediaQueries.mobile} {
+    gap: 1.6rem;
 
-        ${typography.font16Bold}
-    }
-
-`
+    ${typography.font14Bold}
+  }
+`;
 
 interface BoxProps {
-    $isSelected?: boolean;
+  $isSelected?: boolean;
 }
 
 const Box = styled.div<BoxProps>`
+  ${({ $isSelected }) => $isSelected && `color: ${color.black[1]};`}
 
-    ${({ $isSelected }) => $isSelected && `color: ${color.black[1]};`}
-
-    &:hover{
-       cursor: pointer; 
-       ${({ $isSelected }) => !$isSelected && `color: ${color.black[3]};`}
-    }
+  &:hover {
+    cursor: pointer;
+    ${({ $isSelected }) => !$isSelected && `color: ${color.black[3]};`}
+  }
 `;
