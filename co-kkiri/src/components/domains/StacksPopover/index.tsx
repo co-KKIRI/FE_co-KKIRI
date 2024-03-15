46ef5b3068a46951eee44371c0d43b63c85575ec
이나경 <jay3925@cau.ac.kr>
2024-03-16 03:47:22 +0900

    Fix: StackPopover 컴포넌트 position 변경

--- co-kkiri/src/components/domains/StacksPopover/index.tsx
+++ co-kkiri/src/components/domains/StacksPopover/index.tsx
@@ -1,16 +1,22 @@
 import FilterButton from "@/components/commons/FilterButton";
 import { useState } from "react";
 import SelectLayout from "./SelectLayout";
+import styled from "styled-components";
 
 export default function StacksPopover() {
-    const [isButtonSelected, setIsButtonSelected] = useState<boolean>(false);
-    const handleButtonClick = () => {
-        setIsButtonSelected(!isButtonSelected);
-    }
+  const [isButtonSelected, setIsButtonSelected] = useState<boolean>(false);
+  const handleButtonClick = () => {
+    setIsButtonSelected(!isButtonSelected);
+  };
 
-    return (<div>
-        <FilterButton selectOption="기술 스택" isSelected={isButtonSelected} onClick={handleButtonClick} />
-        {isButtonSelected && <SelectLayout />}
-    </div>
-    )
+  return (
+    <Container>
+      <FilterButton selectOption="기술 스택" isSelected={isButtonSelected} onClick={handleButtonClick} />
+      {isButtonSelected && <SelectLayout />}
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  position: relative;
+`;
