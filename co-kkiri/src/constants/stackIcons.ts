3aa67472c5abd1286d3a5d700e50d81b16b38ed3
이나경 <jay3925@cau.ac.kr>
2024-03-15 16:02:05 +0900

    Chore: stackIcons.ts 삭제

--- co-kkiri/src/constants/stackIcons.ts
+++
@@ -1,8 +0,0 @@
-interface StackIcons {
-  [key: string]: {
-    src: string;
-    alt: string;
-  };
-}
-
-export const STACK_ICONS: StackIcons = { HTML: { src: "https://simpleicons.org/icons/html5.svg", alt: "HTML 아이콘" } };
