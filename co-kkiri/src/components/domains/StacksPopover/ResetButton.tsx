090b219e829cc1214827be7ba5bcbb546a73ed47
이나경 <jay3925@cau.ac.kr>
2024-03-16 03:25:30 +0900

    Feat: ResetButton 구현

---
+++ co-kkiri/src/components/domains/StacksPopover/ResetButton.tsx
@@ -0,0 +1,52 @@
+import { ICONS } from "@/constants/icons";
+import DESIGN_TOKEN from "@/styles/tokens";
+import styled from "styled-components";
+
+interface ResetButtonProps {
+  onReset: () => void;
+}
+
+export default function ResetButton({ onReset }: ResetButtonProps) {
+  return (
+    <Container onClick={onReset}>
+      <img src={ICONS.reset.src} alt={ICONS.reset.alt} />
+      <p>초기화</p>
+    </Container>
+  );
+}
+
+const { color, typography } = DESIGN_TOKEN;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  color: ${color.black[3]};
+
+  ${typography.font12Semibold}
+
+  img {
+    width: 1.4rem;
+    height: 1.4rem;
+    fill: ${color.black[3]};
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: ${color.black[1]};
+
+    img {
+      animation: rotateAnimation 1s ease-in-out infinite;
+    }
+  }
+
+  @keyframes rotateAnimation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(-360deg);
+    }
+  }
+`;
