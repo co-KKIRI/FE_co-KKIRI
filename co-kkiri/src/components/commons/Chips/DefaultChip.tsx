ae4e5337f62b6fb7821d97c4f2c4208a9f513ae6
이나경 <jay3925@cau.ac.kr>
2024-03-16 11:42:03 +0900

    Fix: DefaultChip title 툴팁 추가

--- co-kkiri/src/components/commons/Chips/DefaultChip.tsx
+++ co-kkiri/src/components/commons/Chips/DefaultChip.tsx
@@ -32,6 +32,7 @@ export default function DefaultChip({
 }: DefaultChipProps) {
   return (
     <Container
+      title={label}
       className={className}
       $isVertical={isVertical}
       $isSelected={isSelected}
