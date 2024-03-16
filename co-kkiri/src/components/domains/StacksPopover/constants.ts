844edf9cf84bb2bd52ef4739ac7b7bd63b95ff09
이나경 <jay3925@cau.ac.kr>
2024-03-16 11:15:35 +0900

    Chore: StacksPopover에 사용되는 constant 파일 생성

---
+++ co-kkiri/src/components/domains/StacksPopover/constants.ts
@@ -0,0 +1,11 @@
+import { StackPosition } from "@/types/StackTypes";
+
+export type StackPositionFilter = "ALL" | StackPosition;
+
+export const mappedFilter: Record<string, StackPositionFilter> = {
+  "전체": "ALL",
+  "프론트엔드": "FRONT_END",
+  "백엔드": "BACK_END",
+  "모바일": "MOBILE",
+  "기타": "OTHERS",
+};
