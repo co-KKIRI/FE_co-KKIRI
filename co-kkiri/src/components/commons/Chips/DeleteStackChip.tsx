cb538dae163438596217806ce5ff23371eadbcd9
이나경 <jay3925@cau.ac.kr>
2024-03-16 10:58:12 +0900

    Fix: DeleteStackChip onClick으로 prop넘긴 오류 수정

--- co-kkiri/src/components/commons/Chips/DeleteStackChip.tsx
+++ co-kkiri/src/components/commons/Chips/DeleteStackChip.tsx
@@ -4,15 +4,18 @@ import { MouseEvent } from "react";
 import { ICONS } from "@/constants/icons";
 
 interface DeleteStackChipProps {
-    label: string;
-    onClick: (e: MouseEvent<HTMLDivElement>) => void;
+  label: string;
+  onDelete: (stack: string) => void;
 }
 
-export default function DeleteStackChip({ label, onClick }: DeleteStackChipProps
-) {
-    return <Container label={label} icon={ICONS.deleted} onClick={onClick} />;
+export default function DeleteStackChip({ label, onDelete }: DeleteStackChipProps) {
+  const onClick = (e: MouseEvent<HTMLDivElement>) => {
+    onDelete(label);
+  };
+
+  return <Container label={label} icon={ICONS.deleted} onIconClick={onClick} />;
 }
 
 const Container = styled(DefaultChip)`
-    padding: 0.4rem .8rem .4rem 1.2rem;
-`
+  padding: 0.4rem 0.8rem 0.4rem 1.2rem;
+`;
