4d7319246e6974b8c5daab1de0f40301a00184cb
이나경 <jay3925@cau.ac.kr>
2024-03-16 14:15:56 +0900

    Refactor: StackPopover 컴포넌트 변경된 하위 컴포넌트 이름 반영

--- co-kkiri/src/components/domains/detail/StackPopover/index.tsx
+++ co-kkiri/src/components/domains/detail/StackPopover/index.tsx
@@ -1,4 +1,4 @@
-import FilterButton from "@/components/commons/FilterButton";
+import FilterDropdown from "@/components/commons/DropDowns/commons/FilterButton";
 import SelectLayout from "./SelectLayout";
 import styled from "styled-components";
 import useOpenToggle from "@/hooks/useOpenToggle";
@@ -12,7 +12,7 @@ export default function StacksPopover({ onStacksChange }: StacksPopoverProps) {
 
   return (
     <Container>
-      <FilterButton selectOption="기술 스택" isSelected={isOpen} onClick={openToggle} />
+      <FilterDropdown selectOption="기술 스택" isSelected={isOpen} onClick={openToggle} />
       {isOpen && <SelectLayout onStacksChange={onStacksChange} />}
     </Container>
   );
