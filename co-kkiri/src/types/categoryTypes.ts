83b14e43e9dd56a9e29e10a0f4f917e2e106e923
Yoon-kyungLee <yk2588@nate.com>
2024-03-17 22:22:58 +0900

    Feat: Category 컴포넌트 삭제하고 FilterList 컴포넌트에 합침

---
+++ co-kkiri/src/types/categoryTypes.ts
@@ -0,0 +1,5 @@
+export type CategoryList = "STUDY" | "PROJECT";
+
+// 나의 스터디 타입 고민(CategoryMyStudy)
+// mystudy: ["내가 신청한", "모집중", "진행중", "완료"]
+// "AppliedStudy" | "RecruitedStudy" | "OngoingStudy" | "CompletedStudy"
