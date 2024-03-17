9ff553667d45448673d02cef293533bbcf56b1ad
김주현 <trust987@naver.com>
2024-03-18 04:24:35 +0900

    Chore: mock data 수정 및 api 명세에 따라 타입 수정

--- co-kkiri/src/lib/mock/commentList.ts
+++ co-kkiri/src/lib/mock/commentList.ts
@@ -1,4 +1,4 @@
-interface CommentInfo {
+export interface CommentInfo {
   commentId: number;
   commentProfileImg: string;
   commentNickname: string;
@@ -7,14 +7,20 @@ interface CommentInfo {
   isMine: boolean;
 }
 
-export const commentList = {
+interface CommentList {
+  result: {
+    comments: CommentInfo[];
+  };
+}
+
+export const commentList: CommentList = {
   result: {
     comments: [
       {
         commentId: 1,
         commentProfileImg: "",
         commentNickname: "호랑이",
-        commentCreatedAt: "2024-03-14T12:30:00",
+        commentCreatedAt: "2024-03-14 12:30:00",
         commentContent: "스터디에 참여하고 싶어요!",
         isMine: false,
       },
@@ -22,7 +28,7 @@ export const commentList = {
         commentId: 2,
         commentProfileImg: "",
         commentNickname: "사자",
-        commentCreatedAt: "2024-03-14T13:00:00",
+        commentCreatedAt: "2024-03-14 13:00:00",
         commentContent: "어떤 주제로 진행하나요?",
         isMine: false,
       },
@@ -30,7 +36,7 @@ export const commentList = {
         commentId: 3,
         commentProfileImg: "",
         commentNickname: "기린",
-        commentCreatedAt: "2024-03-14T13:30:00",
+        commentCreatedAt: "2024-03-14 13:30:00",
         commentContent: "온라인으로만 진행하나요, 혹은 오프라인도 가능한가요?",
         isMine: false,
       },
@@ -38,7 +44,7 @@ export const commentList = {
         commentId: 4,
         commentProfileImg: "",
         commentNickname: "코끼리",
-        commentCreatedAt: "2024-03-14T14:00:00",
+        commentCreatedAt: "2024-03-14 14:00:00",
         commentContent: "오프라인도 가능합니다!",
         isMine: true,
       },
@@ -46,7 +52,7 @@ export const commentList = {
         commentId: 5,
         commentProfileImg: "",
         commentNickname: "기린",
-        commentCreatedAt: "2024-03-14T14:30:00",
+        commentCreatedAt: "2024-03-14 14:30:00",
         commentContent: "스터디 인원은 어느 정도 인가요?",
         isMine: false,
       },
@@ -54,24 +60,8 @@ export const commentList = {
         commentId: 6,
         commentProfileImg: "",
         commentNickname: "판다",
-        commentCreatedAt: "2024-03-14T15:00:00",
+        commentCreatedAt: "2024-03-14 15:00:00",
         commentContent: "스터디 기간은 얼마나 되나요?",
-        isMine: false,
-      },
-      {
-        commentId: 7,
-        commentProfileImg: "",
-        commentNickname: "늑대",
-        commentCreatedAt: "2024-03-14T15:30:00",
-        commentContent: "다른 스터디 경험이 있어야 하나요?",
-        isMine: false,
-      },
-      {
-        commentId: 8,
-        commentProfileImg: "",
-        commentNickname: "펭귄",
-        commentCreatedAt: "2024-03-14T16:00:00",
-        commentContent: "관심 있습니다! 어떻게 신청하나요?",
         isMine: false,
       },
     ],
--- co-kkiri/src/lib/mock/studyDetail.ts
+++ co-kkiri/src/lib/mock/studyDetail.ts
@@ -1,6 +1,30 @@
-export const studyDetailData = {
+export interface DetailInfo {
+  postTitle: string; //제목
+  postContent: string; //내용
+  userProfileImg: string; //작성자 프로필이미지
+  userNickname: string; //작성자 닉네임
+  createdAt: string; //생성 시간 YYYY-MM-DD HH:MM:SS
+  views: number; //조회수
+  isScraped: boolean; //스크랩 되어있는지 아닌지
+  scraps: number; //스크랩수
+  type: "STUDY" | "PROJECT"; //스터디 종류
+  recruitEndAt: string; // 모집 마감
+  progressPeriod: string; //진행 기간 => enum 타입으로 넣을 예정
+  progressWay: string; //진행 방법 (온라인/오프라인)
+  contactWay: string; //연락 방법
+  capacity: number; //모집 인원 ==> 테이블에 추가 예정
+  positions: string[]; //포지션
+  stacks: string[]; //스택
+  commentsNum: number;
+}
+
+interface StudyDetailData {
+  result: DetailInfo;
+}
+
+export const studyDetailData: StudyDetailData = {
   result: {
-    postTitle: "실제 사용할 쇼핑몰 웹프로젝트 만들어나가실 분 구합니다. ", //제목
+    postTitle: "실제 사용할 쇼핑몰 웹프로젝트 만들어나가실 분 구합니다.", //제목
     postContent: `<section>
     <h1>모집 소개</h1>
     <p>함께 프로젝트를 진행할 디자이너분과 플러터 개발자분을 추가로 모집합니다!</p>
@@ -22,13 +46,27 @@ export const studyDetailData = {
     <p>현재 성공적인 프로젝트 마무리를 위해 3만원 디파짓을 걷고 있으니 이점 참고해주시길 바랍니다.</p>
     <p>같이 열심히 해서 모두에게 좋은 포트폴리오 만들어봐요 :)</p>
     <p>많은 지원 부탁드립니다 :)</p>
+    <h2>모집 인원</h2>
+    <p>현재 디자이너 1명, 플러터 개발자 1명(프론트)을 모집하고 있으며, 디자인은 피그마를 사용할 예정입니다.</p>
+    <p>개인 프로젝트도 좋으니 프로젝트 경험이 한번이라도 있으신 분들이 지원해주셨으면 좋겠습니다.</p>
+    <p>또한 정말 열심히 하고 싶기 때문에, 대면 만남이 가능하신 분을 희망합니다!</p>
+    <p>현재 성공적인 프로젝트 마무리를 위해 3만원 디파짓을 걷고 있으니 이점 참고해주시길 바랍니다.</p>
+    <p>같이 열심히 해서 모두에게 좋은 포트폴리오 만들어봐요 :)</p>
+    <p>많은 지원 부탁드립니다 :)</p>
+    <h2>모집 인원</h2>
+    <p>현재 디자이너 1명, 플러터 개발자 1명(프론트)을 모집하고 있으며, 디자인은 피그마를 사용할 예정입니다.</p>
+    <p>개인 프로젝트도 좋으니 프로젝트 경험이 한번이라도 있으신 분들이 지원해주셨으면 좋겠습니다.</p>
+    <p>또한 정말 열심히 하고 싶기 때문에, 대면 만남이 가능하신 분을 희망합니다!</p>
+    <p>현재 성공적인 프로젝트 마무리를 위해 3만원 디파짓을 걷고 있으니 이점 참고해주시길 바랍니다.</p>
+    <p>같이 열심히 해서 모두에게 좋은 포트폴리오 만들어봐요 :)</p>
+    <p>많은 지원 부탁드립니다 :)</p>
 </section>`, //내용
     userProfileImg: "", //작성자 프로필이미지
     userNickname: "코끼리", //작성자 닉네임
-    createdAt: "2024-03-05", //생성 시간 YYYY-M.-D.
+    createdAt: "2024-03-05 13:00:00", //생성 시간 YYYY-M.-D.
     views: 3, //조회수
     isScraped: false, //스크랩 되어있는지 아닌지
-    scraps: 24, //스크랩수
+    scraps: 22, //스크랩수
     type: "PROJECT", //스터디 종류
     recruitEndAt: "2024.3.8", // 모집 마감
     progressPeriod: "2개월", //진행 기간 => enum 타입으로 넣을 예정
@@ -37,6 +75,6 @@ export const studyDetailData = {
     capacity: 2, //모집 인원 ==> 테이블에 추가 예정
     positions: ["프론트엔드", "안드로이드", "디자이너"], //포지션
     stacks: ["AWS", "React", "Node.js"], //스택
-    commentsNum: 4, //댓글 수
+    commentsNum: 6, //댓글 수
   },
 };
