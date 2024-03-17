e32e2a1f608cf2d6c67b275935216c03624449f3
Yoon-kyungLee <yk2588@nate.com>
2024-03-17 16:26:35 +0900

    Chore: 경로 오류 수정

--- co-kkiri/src/components/commons/Chips/ProjectChip.tsx
+++ co-kkiri/src/components/commons/Chips/ProjectChip.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 import DefaultChip from "./DefaultChip";
 import { VariantStyle } from "@/types/styledUtilTypes";
-import { getTypeLabel } from "@/utils/getTypeLable";
+import { getTypeLabel } from "@/utils/getTypeLabel";
 
 export type ProjectType = "STUDY" | "PROJECT";
 
