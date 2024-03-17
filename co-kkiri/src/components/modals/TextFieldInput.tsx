17d73fddd379e59d521a5d732440042dee6ffc47
Haeun <han.kim0904@gmail.com>
2024-03-17 19:01:49 +0900

    Fix: placeholder 추가, JSDoc 작성, 에러 메세지에 스타일 추가

--- co-kkiri/src/components/modals/TextFieldInput.tsx
+++ co-kkiri/src/components/modals/TextFieldInput.tsx
@@ -1,27 +1,25 @@
 import { InputHTMLAttributes, forwardRef } from "react";
+import { FieldError } from "react-hook-form";
 import { styled } from "styled-components";
 import DESIGN_TOKEN from "@/styles/tokens";
 
 interface TextFieldInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
+  label: string;
+  isRequired?: boolean;
+  isError: FieldError | undefined;
   helperText?: string;
 }
 
 const TextFieldInput = forwardRef<HTMLInputElement, TextFieldInputProps>(
-  ({ label, id, placeholder, value, maxLength, helperText, onChange, onBlur }, ref) => {
+  ({ label, id, value, placeholder = "", isRequired = false, isError, helperText = "", ...props }, ref) => {
     return (
       <Wrapper>
-        <Label htmlFor={id}>{label}</Label>
-        <Input
-          id={id}
-          placeholder={placeholder}
-          value={value}
-          maxLength={maxLength}
-          onChange={onChange}
-          onBlur={onBlur}
-          ref={ref}
-        />
-        <p>{helperText}</p> {/*아직 스타일은 적용하지 않음*/}
+        <Label htmlFor={id}>
+          {label}
+          {isRequired && <span className="required-mark"> *</span>}
+        </Label>
+        <Input $isError={isError} id={id} value={value} placeholder={placeholder} ref={ref} {...props} />
+        <Text>{helperText}</Text>
       </Wrapper>
     );
   },
@@ -31,24 +29,29 @@ export default TextFieldInput;
 
 const {
   color,
-  typography: { font16Bold, font16Medium },
+  typography: { font12Medium, font16Bold, font16Medium },
 } = DESIGN_TOKEN;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.2rem;
+  gap: 0.8rem;
 `;
 
 const Label = styled.label`
   color: ${color.black[1]};
   ${font16Bold};
+  margin-bottom: 0.4rem;
+
+  & .required-mark {
+    color: ${color.red};
+  }
 `;
 
-const Input = styled.input`
+const Input = styled.input<{ $isError: FieldError | undefined }>`
   ${font16Medium};
   background: ${color.white};
-  border: 1px solid ${color.gray[2]};
+  border: 1px solid ${({ $isError }) => ($isError ? color.red : color.gray[2])};
   border-radius: 0.5rem;
   color: ${color.black[1]};
   padding: 1.4rem 2rem 1.5rem;
@@ -57,4 +60,14 @@ const Input = styled.input`
   &:focus {
     outline: none;
   }
+
+  &::placeholder {
+    color: ${color.gray[2]};
+  }
+`;
+
+const Text = styled.p`
+  ${font12Medium}
+  color: ${color.red};
+  height: 1.4rem;
 `;
