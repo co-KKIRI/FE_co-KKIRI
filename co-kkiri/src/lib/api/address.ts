92b096064190dd1ee9fc845cd8e321065551f5b2
Joohyun Kim <trust987@naver.com>
2024-03-20 02:17:18 +0900

    Feat: axios 기본 셋팅

---
+++ co-kkiri/src/lib/api/address.ts
@@ -0,0 +1,89 @@
+//get
+export const homeAddress = "/main/list";
+
+export const PostAddress = {
+  //post
+  recruit: "/post/recruit",
+  //get
+  list: "/post/list",
+  //get, delete
+  postId: (postId: number) => `/post/${postId}`,
+  //get, post
+  apply: (postId: number) => `/post/${postId}/apply`,
+  //patch
+  modify: (postId: number) => `/post/${postId}/modify`,
+  //get
+  management: (postId: number) => `/post/${postId}/management`,
+  //patch
+  recruitEnd: (postId: number) => `/post/${postId}/recruit-end`,
+  //patch
+  recruitStart: (postId: number) => `/post/${postId}/recruit-start`,
+  //patch
+  recruitComplete: (postId: number) => `/post/${postId}/recruit-complete`,
+};
+
+export const teamMemberAddress = {
+  //get
+  teamMember: (postId: number) => `/post/${postId}/team-member`,
+  //patch
+  accept: (teamMemberId: number) => `/post/team-member/${teamMemberId}/accept`,
+  //patch
+  reject: (teamMemberId: number) => `/post/team-member/${teamMemberId}/reject`,
+  //delete
+  out: (teamMemberId: number) => `/post/team-member/${teamMemberId}/out`,
+};
+
+export const commentAddress = {
+  //get
+  list: (postId: number) => `/post/${postId}/comment/list`,
+  //post
+  write: (postId: number) => `/post/${postId}/comment/write`,
+  //patch, delete
+  commentId: (postId: number, commentId: number) => `/post/${postId}/${commentId}`,
+};
+
+//post, delete
+export const scrapAddress = (postId: number) => `/post/${postId}/scrap`;
+
+export const myPageAddress = {
+  //get, patch
+  userInfo: "/my-page/info",
+  //get
+  inviteList: "/my-page/invite/list",
+  //get
+  scrapList: "/my-page/scrap/list",
+  //patch
+  visibleProfile: "/my-page/visible-profile",
+  //delete
+  myPage: "/my-page",
+};
+
+export const teamAddress = {
+  //patch
+  accept: "/team/accept",
+  //patch
+  reject: "/team/reject",
+};
+
+export const MyPostAddress = {
+  //get
+  applyList: "/my-post/apply/list",
+  //get
+  recruitList: "/my-post/recruit/list",
+  //get
+  onGoingList: "/my-post/on-going/list",
+  //get
+  completedList: "/my-post/complete/list",
+  //patch
+  onGoingComplete: "/my-post/on-going/complete",
+  //post
+  review: "/my-post/complete/review",
+  //get
+  reviewInfo: "/my-post/complete/review/info",
+};
+
+//get
+export const memberAddress = { memberId: (memberId: number) => `/member/${memberId}`, search: "/member/search" };
+
+//post
+export const imageAddress = "/image";
