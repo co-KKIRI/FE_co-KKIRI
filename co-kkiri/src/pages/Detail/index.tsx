a6dac901b127e4706f9348ddbc91e9475dc8815f
Joohyun Kim <trust987@naver.com>
2024-03-19 19:17:29 +0900

    Feat: 데이터에 따라 유동적인 카드 컴포넌트 길이 측정하여 sticky 버튼 top 위치 조정

--- co-kkiri/src/pages/Detail/index.tsx
+++ co-kkiri/src/pages/Detail/index.tsx
@@ -1,8 +1,12 @@
+import { useRef } from "react";
 import * as S from "./styled";
-import { studyDetailData } from "@/lib/mock/studyDetail";
+import { DetailInfo, studyDetailData } from "@/lib/mock/studyDetail";
+import useComponentHeight from "@/hooks/useComponentHeight";
 
 export default function Detail() {
   const detailInfo = studyDetailData.result;
+  const cardRef = useRef<HTMLDivElement>(null);
+  const cardHeight = useComponentHeight<DetailInfo>(detailInfo, cardRef, 407);
 
   return (
     <S.Container>
@@ -10,9 +14,9 @@ export default function Detail() {
         <S.GoBackButton />
         <S.ShareAndScrapButton isScraped={detailInfo.isScraped} />
         <S.PostSection detailInfo={detailInfo} />
-        <S.DetailCardSection detailInfo={detailInfo} />
+        <S.DetailCardSection cardRef={cardRef} detailInfo={detailInfo} />
         <S.CommentsSection commentsNum={detailInfo.commentsNum} />
-        <S.MappedButton variant="primary">지원하기</S.MappedButton>
+        <S.ButtonSection $cardHeight={cardHeight} />
         {/* 로직에 따라서 버튼 표시여부, 내부 텍스트 및 색상, 클릭 시 각 버튼에 해당하는 기능 맵핑 예정 */}
       </S.Box>
     </S.Container>
