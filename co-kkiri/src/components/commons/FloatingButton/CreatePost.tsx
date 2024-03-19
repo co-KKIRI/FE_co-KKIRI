5028da4c75abd13e24f685186116fc4c088274ec
Joohyun Kim <trust987@naver.com>
2024-03-20 00:05:34 +0900

    Feat: CreatePost 버튼 기능 구현

--- co-kkiri/src/components/commons/FloatingButton/CreatePost.tsx
+++ co-kkiri/src/components/commons/FloatingButton/CreatePost.tsx
@@ -2,9 +2,16 @@ import styled from "styled-components";
 import Button from "../Button";
 import DESIGN_TOKEN from "@/styles/tokens";
 import { ICONS } from "@/constants/icons";
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
-  return <Container variant="floating" icon={ICONS.post} />;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/post`);
+  };
+
+  return <Container variant="floating" icon={ICONS.post} onClick={handleClick} />;
 }
 
 const { color, mediaQueries } = DESIGN_TOKEN;
