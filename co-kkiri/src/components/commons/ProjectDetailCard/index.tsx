23f3a7384010084752235c9081d6a993f128c5f6
이나경 <jay3925@cau.ac.kr>
2024-03-20 00:02:43 +0900

    Chore: Prettier에 맞춰 조정

--- co-kkiri/src/components/commons/ProjectDetailCard/index.tsx
+++ co-kkiri/src/components/commons/ProjectDetailCard/index.tsx
@@ -6,91 +6,93 @@ import { ProjectDetailConfig, ProjectDetailContentType } from "./types";
 import ProjectDetailRow from "./ProjectDetailRow";
 
 interface ProjectDetailCardProps extends ProjectDetailContentType {
-    ProjectCategory: ProjectType;
+  ProjectCategory: ProjectType;
 }
 
-
 export default function ProjectDetailCard({ ProjectCategory, ...projectDetailContents }: ProjectDetailCardProps) {
+  // 이 친구가 여기 있으면 안될 것 같은데 추후에 코드를 수정해서 밖으로 빼던 해야할 듯 합니다
+  const projectDetailConfig: ProjectDetailConfig = {
+    recruitEndAt: {
+      label: "모집 마감",
+      content: projectDetailContents.recruitEndAt,
+      renderType: "text",
+    },
+    progressPeriod: {
+      label: "진행 기간",
+      content: projectDetailContents.progressPeriod,
+      renderType: "text",
+    },
+    progressWay: {
+      label: "진행 방식",
+      content: projectDetailContents.progressWay,
+      renderType: "text",
+    },
+    contactWay: {
+      label: "연락 방식",
+      content: projectDetailContents.contactWay,
+      renderType: "text",
+    },
+    capacity: {
+      label: "모집 인원",
+      content: projectDetailContents.capacity,
+      renderType: "text",
+    },
+    positions: {
+      label: "모집 포지션",
+      content: projectDetailContents.positions,
+      renderType: "positionChip",
+    },
+    stacks: {
+      label: "기술 스택",
+      content: projectDetailContents.stacks,
+      renderType: "stackIcon",
+    },
+  };
 
-    // 이 친구가 여기 있으면 안될 것 같은데 추후에 코드를 수정해서 밖으로 빼던 해야할 듯 합니다
-    const projectDetailConfig: ProjectDetailConfig = {
-        'recruitEndAt': {
-            "label": "모집 마감",
-            "content": projectDetailContents.recruitEndAt,
-            "renderType": "text",
-        },
-        'progressPeriod':{
-            "label": "진행 기간",
-            "content": projectDetailContents.progressPeriod,
-            "renderType": "text",
-        },
-        'progressWay':{
-            "label": "진행 방식",
-            "content": projectDetailContents.progressWay,
-            "renderType": "text",
-        },
-        'contactWay':{
-            "label": "연락 방식",
-            "content": projectDetailContents.contactWay,
-            "renderType": "text",
-        },
-        'capacity':{
-            "label": "모집 인원",
-            "content": projectDetailContents.capacity,
-            "renderType": "text",
-        },
-        'positions':{
-            "label": "모집 포지션",
-            "content": projectDetailContents.positions,
-            "renderType": "positionChip",
-        },
-        'stacks':{
-            "label": "기술 스택",
-            "content": projectDetailContents.stacks,
-            "renderType": "stackIcon",
-        }
-    }
-
-    return <Container>
-        <Box><ProjectChip label={ProjectCategory} /></Box>
-        <ProjectDetailTable>
-            {Object.values(projectDetailConfig).map((value) => {
-                return <ProjectDetailRow {...value} />
-            })}
-        </ProjectDetailTable>
-    </Container>;
+  return (
+    <Container>
+      <Box>
+        <ProjectChip label={ProjectCategory} />
+      </Box>
+      <ProjectDetailTable>
+        {Object.values(projectDetailConfig).map((value) => {
+          return <ProjectDetailRow {...value} />;
+        })}
+      </ProjectDetailTable>
+    </Container>
+  );
 }
 
 const { color, boxShadow, mediaQueries } = DESIGN_TOKEN;
 
 const Container = styled.div`
-    height: fit-content;
+  height: fit-content;
 
-    padding: 8rem 3rem 3rem;
-    position: relative;
+  padding: 8rem 3rem 3rem;
+  position: relative;
 
-    background-color: ${color.white};
+  background-color: ${color.white};
 
-    border-radius: 1.5rem;
-    box-shadow: ${boxShadow.content};
+  border-radius: 1.5rem;
+  box-shadow: ${boxShadow.content};
 
-    ${mediaQueries.mobile}{
-        width: 32rem;
-    }
+  ${mediaQueries.mobile} {
+    width: 32rem;
+  }
 
-    ${mediaQueries.tablet}{
-        width: 50rem;
-    }
+  ${mediaQueries.tablet} {
+    width: 50rem;
+  }
 
-    ${mediaQueries.desktop}{
-        width: 35rem;
-    }
-`
+  ${mediaQueries.desktop} {
+    width: 35rem;
+  }
+`;
 
 const Box = styled.div`
-    position: absolute;
-    top: 3rem;
-    left: -0.2rem;
-    width: fit-content;
-    height: fit-content;
-`
+  position: absolute;
+  top: 3rem;
+  left: -0.2rem;
+  width: fit-content;
+  height: fit-content;
+`;
--- co-kkiri/src/components/commons/ProjectDetailCard/types.ts
+++ co-kkiri/src/components/commons/ProjectDetailCard/types.ts
@@ -1,21 +1,28 @@
 import { GetTypeFromObject } from "@/types/ObjectUtilTypes";
 
 // ProjectDetailCard내 Table에서 사용되는 Key
-export type ProjectDetailKey = "recruitEndAt" | "progressPeriod" | "progressWay" | "contactWay" | "capacity" |"positions" | "stacks";
+export type ProjectDetailKey =
+  | "recruitEndAt"
+  | "progressPeriod"
+  | "progressWay"
+  | "contactWay"
+  | "capacity"
+  | "positions"
+  | "stacks";
 
 // 구체적인 렌더링 방식
 export type RenderType = "text" | "positionChip" | "stackIcon";
 
 // ProjectDetailCard 내부에서 Table에게 전달할 Table Config
 export type ProjectDetailConfig = {
-    [key in ProjectDetailKey]: {
-        label: string;
-        content: string | string[];
-        renderType: RenderType;
-    }
-}
+  [key in ProjectDetailKey]: {
+    label: string;
+    content: string | string[];
+    renderType: RenderType;
+  };
+};
 
 // ProjectDetailTable에서 사용할 content는 Card 외부에서 받을 예정, 따라서 타입에서 필요한 정보만 추출
 export type ProjectDetailContentType = {
-    [key in ProjectDetailKey]: GetTypeFromObject<ProjectDetailConfig[key], "content">;
-}
+  [key in ProjectDetailKey]: GetTypeFromObject<ProjectDetailConfig[key], "content">;
+};
