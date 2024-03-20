bfb9d203bbaf41f168532bad5a687d1782437f98
이나경 <jay3925@cau.ac.kr>
2024-03-21 08:52:01 +0900

    Fix: ModalLayout 재구성 및 form 적용

--- co-kkiri/src/components/modals/EditUserProfileModal/EditUserProfileModalLayout.tsx
+++ co-kkiri/src/components/modals/EditUserProfileModal/EditUserProfileModalLayout.tsx
@@ -1,50 +1,76 @@
 import styled from "styled-components";
-import DefaultInfoField from "./InfoField";
-import MultiselectDropdown from "@/components/commons/DropDowns/StackMultiselectDropdown";
-import ModalTextFieldInput from "../ModalTextFieldInput";
+import DefaultModalTextFieldInput from "../ModalTextFieldInput";
+import DefaultModalTextFieldDropdown from "./ModalTextFieldDropdown";
 import { useForm } from "react-hook-form";
-import UserInfoDropdown from "@/components/commons/DropDowns/UserInfoDropdown";
 import Button from "@/components/commons/Button";
+import DESIGN_TOKEN from "@/styles/tokens";
+export interface FormData {
+  nickname: string;
+  position: string;
+  career: string;
+  link: string;
+  stacks: string[];
+  introduce: string;
+}
 
 export default function EditUserProfileModalLayout() {
-  const { control } = useForm();
+  const { control, watch } = useForm<FormData>({
+    defaultValues: {
+      nickname: "",
+      position: "",
+      career: "",
+      link: "",
+      stacks: [],
+      introduce: "",
+    },
+    mode: "onBlur",
+  });
 
   return (
     <Container>
-      <InfoField
-        label="닉네임"
-        fleid={<ModalTextFieldInput name="nickname" control={control} />}
-        isRequired
-        $gridArea="nickname"
-      />
-      <InfoField label="포지션" fleid={<UserInfoDropdown menuInfoType="position" />} $gridArea="position" />
-      <InfoField label="경력" fleid={<UserInfoDropdown menuInfoType="career" />} $gridArea="career" />
-      <InfoField label="대표 링크" fleid={<ModalTextFieldInput name="link" control={control} />} $gridArea="link" />
-      <InfoField
-        label={"관심 스택"}
-        fleid={<MultiselectDropdown selectedOptions={["JavaScript"]} onSelectChange={() => {}} />}
-        isRequired
-        $gridArea="stack"
-      />
-      <InfoField
-        label={"한줄 소개"}
-        fleid={<ModalTextFieldInput name="introduce" control={control} />}
-        $gridArea="introduce"
-      />
-      <Button variant="primary">지원하기</Button>
+      <ModalTextFieldInput name="nickname" control={control} $gridArea="nickname" />
+      <ModalTextFieldDropdown name="position" control={control} $gridArea="position" />
+      <ModalTextFieldDropdown name="career" control={control} $gridArea="career" />
+      <ModalTextFieldInput name="link" control={control} $gridArea="link" />
+      <ModalTextFieldDropdown name="stacks" control={control} $gridArea="stack" />
+      <ModalTextFieldInput name="introduce" control={control} $gridArea="introduce" />
+      <SubmitButton
+        variant="primary"
+        onClick={() => {
+          console.log(watch());
+        }}
+        $gridArea="button">
+        수정하기
+      </SubmitButton>
     </Container>
   );
 }
+
+const { mediaQueries } = DESIGN_TOKEN;
 
 const Container = styled.section`
   width: 100%;
   display: grid;
   gap: 1.8rem 2rem;
 
-  grid-template-areas: "nickname position" "link career" "stack stack" "introduce introduce";
+  grid-template-areas: "nickname position" "link career" "stack stack" "introduce introduce" "button button";
   grid-template-columns: 1fr 1fr;
+
+  ${mediaQueries.mobile} {
+    display: flex;
+    flex-direction: column;
+    height: 80%;
+  }
 `;
 
-const InfoField = styled(DefaultInfoField)<{ $gridArea: string }>`
+const ModalTextFieldInput = styled(DefaultModalTextFieldInput)<{ $gridArea: string }>`
+  grid-area: ${({ $gridArea }) => $gridArea};
+`;
+
+const ModalTextFieldDropdown = styled(DefaultModalTextFieldDropdown)<{ $gridArea: string }>`
+  grid-area: ${({ $gridArea }) => $gridArea};
+`;
+
+const SubmitButton = styled(Button)<{ $gridArea: string }>`
   grid-area: ${({ $gridArea }) => $gridArea};
 `;
