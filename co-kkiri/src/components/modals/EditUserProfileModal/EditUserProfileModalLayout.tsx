54b7dc79b8b7a5a5840ed7f4faf649c5710b0ffa
이나경 <jay3925@cau.ac.kr>
2024-03-21 02:53:37 +0900

    Chore: Layout Grid에 맞춰 조정

--- co-kkiri/src/components/modals/EditUserProfileModal/EditUserProfileModalLayout.tsx
+++ co-kkiri/src/components/modals/EditUserProfileModal/EditUserProfileModalLayout.tsx
@@ -1,26 +1,36 @@
 import styled from "styled-components";
-import InfoField from "./InfoField";
+import DefaultInfoField from "./InfoField";
 import MultiselectDropdown from "@/components/commons/DropDowns/StackMultiselectDropdown";
 import ModalTextFieldInput from "../ModalTextFieldInput";
 import { useForm } from "react-hook-form";
 import UserInfoDropdown from "@/components/commons/DropDowns/UserInfoDropdown";
-import { Info } from "lucide-react";
 import Button from "@/components/commons/Button";
 
 export default function EditUserProfileModalLayout() {
   const { control } = useForm();
+
   return (
     <Container>
-      <InfoField label={"닉네임"} fleid={<ModalTextFieldInput name="nickname" control={control} />} isRequired />
-      <InfoField label={"포지션"} fleid={<UserInfoDropdown menuInfoType="position" />} />
-      <InfoField label="대표 링크" fleid={<ModalTextFieldInput name="link" control={control} />} />
-      <InfoField label="경력" fleid={<UserInfoDropdown menuInfoType="career" />} />
+      <InfoField
+        label="닉네임"
+        fleid={<ModalTextFieldInput name="nickname" control={control} />}
+        isRequired
+        $gridArea="nickname"
+      />
+      <InfoField label="포지션" fleid={<UserInfoDropdown menuInfoType="position" />} $gridArea="position" />
+      <InfoField label="경력" fleid={<UserInfoDropdown menuInfoType="career" />} $gridArea="career" />
+      <InfoField label="대표 링크" fleid={<ModalTextFieldInput name="link" control={control} />} $gridArea="link" />
       <InfoField
         label={"관심 스택"}
         fleid={<MultiselectDropdown selectedOptions={["JavaScript"]} onSelectChange={() => {}} />}
         isRequired
+        $gridArea="stack"
       />
-      <InfoField label={"한줄 소개"} fleid={<ModalTextFieldInput name="introduce" control={control} />} />
+      <InfoField
+        label={"한줄 소개"}
+        fleid={<ModalTextFieldInput name="introduce" control={control} />}
+        $gridArea="introduce"
+      />
       <Button variant="primary">지원하기</Button>
     </Container>
   );
@@ -29,4 +39,12 @@ export default function EditUserProfileModalLayout() {
 const Container = styled.section`
   width: 100%;
   display: grid;
+  gap: 1.8rem 2rem;
+
+  grid-template-areas: "nickname position" "link career" "stack stack" "introduce introduce";
+  grid-template-columns: 1fr 1fr;
+`;
+
+const InfoField = styled(DefaultInfoField)<{ $gridArea: string }>`
+  grid-area: ${({ $gridArea }) => $gridArea};
 `;
