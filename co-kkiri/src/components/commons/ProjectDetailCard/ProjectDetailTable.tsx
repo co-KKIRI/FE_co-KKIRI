61b55cbf4e6165089c54823755870e8fccc212b5
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-03-20 11:30:47 +0900

    Fix: prettier 미적용 코드 수정 및 ProjectDetailCard number rendering 추가 (#115)
    * Chore: Prettier에 맞춰 조정
    
    * Fix: ProjectCategory Type: ProjectType => CategoryList
    
    * Chore: ProjectChip CategoryList 적용
    
    * Fix: RenderType "personNumber" 추가
    
    * Chore: Prettier에 맞춰 조정
    
    * Fix: ContentType type 생성 및 대체, number 추가

--- co-kkiri/src/components/commons/ProjectDetailCard/ProjectDetailTable.tsx
+++ co-kkiri/src/components/commons/ProjectDetailCard/ProjectDetailTable.tsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 interface ProjectDetailTableProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export default function ProjectDetailTable({children}: ProjectDetailTableProps) {
-    return <Container>{children}</Container>;
+export default function ProjectDetailTable({ children }: ProjectDetailTableProps) {
+  return <Container>{children}</Container>;
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 2.4rem;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+`;
