cda1fed898d12c41d46666f2cc1ae6854a3339d3
이나경 <jay3925@cau.ac.kr>
2024-03-21 02:02:31 +0900

    Feat: Modal 기본 골조 추가

--- co-kkiri/src/components/modals/EditUserProfileModal/index.tsx
+++ co-kkiri/src/components/modals/EditUserProfileModal/index.tsx
@@ -1,13 +1,19 @@
-import ModalLayout from "../ModalLayout";
+import DefaultModalLayout from "../ModalLayout";
+import EditUserProfileModalLayout from "./EditUserProfileModalLayout";
+import styled from "styled-components";
 
 interface EditUserProfileModalProps {
   onClose: () => void;
 }
 
-export default function EditUserProfileModal({onClose}: EditUserProfileModalProps){
-  return(
+export default function EditUserProfileModal({ onClose }: EditUserProfileModalProps) {
+  return (
     <ModalLayout desktopWidth={708} mobileWidth={320} onClick={onClose} onClose={onClose}>
-      하이
+      <EditUserProfileModalLayout />
     </ModalLayout>
-  )
+  );
 }
+
+const ModalLayout = styled(DefaultModalLayout)`
+  padding: 4rem 3rem 3rem 3rem;
+`;
