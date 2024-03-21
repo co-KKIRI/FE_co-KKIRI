15755f2a57e2a31ec9fe0afd66051efb24450a4f
이윤경 <148832727+Yoon-kyungLee@users.noreply.github.com>
2024-03-21 11:48:45 +0900

    Chore: SideBar 페이지 이동 후 모달 닫기 구현 (#123)
    Co-authored-by: Joohyun Kim <148737398+kimbobby@users.noreply.github.com>

--- co-kkiri/src/components/commons/SideBar/SideBarList.tsx
+++ co-kkiri/src/components/commons/SideBar/SideBarList.tsx
@@ -2,26 +2,41 @@ import { ROUTER_PATH } from "@/lib/path";
 import * as S from "./SideBar.styled";
 import { ICONS } from "@/constants/icons";
 import { Link } from "react-router-dom";
+import useSideBarStore from "@/stores/sideBarStore";
+import { useWindowSize } from "usehooks-ts";
 
 interface SideBarListProps {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export default function SideBarList({ onClick }: SideBarListProps) {
+
   const { HOME_PATH, STUDY_LIST_PATH, SCOUT } = ROUTER_PATH;
+
+  const { width: screenWidth } = useWindowSize();
+  const isTabletOrMobile = screenWidth < 1200;
+
+  const toggleSideBar = useSideBarStore((state) => state.toggleSideBar);
+
+  const toggleSideBarInTabletOrMobile = () => {
+    if (isTabletOrMobile) {
+      toggleSideBar();
+    }
+  };
+
   return (
     <S.Container>
       <S.HamburgerMenuWrapper onClick={onClick}>
         <img src={ICONS.category.src} alt={ICONS.category.alt} />
       </S.HamburgerMenuWrapper>
       <S.CategoryBox>
-        <Link to={HOME_PATH}>
+        <Link to={HOME_PATH} onClick={toggleSideBarInTabletOrMobile}>
           <S.Category>홈</S.Category>
         </Link>
-        <Link to={STUDY_LIST_PATH}>
+        <Link to={STUDY_LIST_PATH} onClick={toggleSideBarInTabletOrMobile}>
           <S.Category>스터디/프로젝트 찾기</S.Category>
         </Link>
-        <Link to={SCOUT}>
+        <Link to={SCOUT} onClick={toggleSideBarInTabletOrMobile}>
           <S.Category>스카우트</S.Category>
         </Link>
       </S.CategoryBox>
