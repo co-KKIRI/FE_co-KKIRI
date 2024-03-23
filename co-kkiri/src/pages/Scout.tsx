14f18f3d8f0efcadc668c8b398021b7524e577fa
Joohyun Kim <trust987@naver.com>
2024-03-24 01:26:02 +0900

    Feat: Scout페이지 레이아웃 구현

--- co-kkiri/src/pages/Scout.tsx
+++ co-kkiri/src/pages/Scout.tsx
@@ -1,3 +1,97 @@
-export default function Scout() {
-  return <div>Scout</div>;
+import { useState } from "react";
+import styled from "styled-components";
+import ScoutFilters from "@/components/domains/scout/ScoutFilters";
+import ScoutCards from "@/components/domains/scout/ScoutCards";
+import { searchedMemberProfiles } from "@/lib/mock/scout/searchMemberList";
+import DESIGN_TOKEN from "@/styles/tokens";
+import SearchInput from "@/components/commons/SearchInput";
+import Pagination from "@/components/commons/Pagination_test/Pagination";
+export interface SelectedFilter {
+  position: string;
+  stack: string[];
 }
+
+export default function Scout() {
+  const [selectedFilter, setSelectedFilter] = useState<SelectedFilter>({
+    position: "",
+    stack: [],
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 2;
+
+  const handlePositionChange = (position: string) => setSelectedFilter((prev) => ({ ...prev, position }));
+
+  const handleStacksChange = (stack: string[]) => setSelectedFilter((prev) => ({ ...prev, stack }));
+  console.log(selectedFilter);
+
+  return (
+    <Container>
+      <Box>
+        <Wrapper>
+          <Title>스카우트</Title> <SearchInput placeholder="멤버를 찾아보세요!" />
+        </Wrapper>
+        <ScoutFilters
+          selectedFilter={selectedFilter}
+          handleStacksChange={handleStacksChange}
+          handlePositionChange={handlePositionChange}
+        />
+        <ScoutCards userProfiles={searchedMemberProfiles} />
+        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
+      </Box>
+    </Container>
+  );
+}
+
+const {
+  spacing,
+  color,
+  typography: { font20Bold },
+  mediaQueries,
+} = DESIGN_TOKEN;
+
+const Container = styled.div`
+  padding: ${spacing.desktop};
+  padding-top: 2.8rem;
+  padding-bottom: 9rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  ${mediaQueries.tablet} {
+    padding: ${spacing.tablet};
+    padding-bottom: 12rem;
+  }
+  ${mediaQueries.mobile} {
+    padding: ${spacing.mobile};
+    padding-bottom: 4.8rem;
+  }
+`;
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 2.8rem;
+
+  ${mediaQueries.mobile} {
+    margin-bottom: 1.8rem;
+  }
+
+  ${mediaQueries.mobile} {
+    flex-direction: column;
+    gap: 2rem;
+    align-items: flex-start;
+    margin-bottom: 1.6rem;
+  }
+`;
+
+const Title = styled.div`
+  ${font20Bold}
+  color:${color.black[1]}
+`;
