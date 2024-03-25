d3378ff5ac66b9a9b7411215634fc55e676c7311
Yoon-kyungLee <yk2588@nate.com>
2024-03-26 03:43:54 +0900

    Chore: 리액트 쿼리 구현

--- co-kkiri/src/pages/Manage/index.tsx
+++ co-kkiri/src/pages/Manage/index.tsx
@@ -4,106 +4,81 @@ import AppliedList from "@/components/domains/manage/AppliedList";
 import { getAppliedMemberList, getStudyManagement } from "@/lib/api/post";
 import { useEffect, useState } from "react";
 import { AppliedMemberListApiResponseDto, StudyManagementApiResponseDto } from "@/lib/api/post/type";
-import { acceptMember, deleteTeamMember, getTeamMember, rejectMember } from "@/lib/api/teamMember";
+import { getTeamMember } from "@/lib/api/teamMember";
 import { TeamMemberApiResponseDto } from "@/lib/api/teamMember/type";
+import { useQuery } from "@tanstack/react-query";
 
 interface ManageProps {
   postId: StudyManagementApiResponseDto["postId"];
 }
 
-export default function Manage({ postId = 1 }: ManageProps) {
+export default function Manage({ postId }: ManageProps) {
   const [detailInfo, setDetailInfo] = useState<StudyManagementApiResponseDto>();
   const [appliedMemberList, setAppliedMemberList] = useState<AppliedMemberListApiResponseDto["data"]>([]);
   const [memberList, setMemberList] = useState<TeamMemberApiResponseDto["data"]>([]);
 
-  const handleAcceptMember = async (teamMemberId: number) => {
-    try {
-      await acceptMember(teamMemberId);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const {
+    data: detailInfoData,
+    error,
+    isError,
+  } = useQuery({
+    queryKey: ["management", postId],
+    queryFn: () => getStudyManagement(postId),
+  });
+  const {
+    data: appliedMemberListData,
+    error: appliedMemberListError,
+    isError: isAppliedMemberListError,
+  } = useQuery({
+    queryKey: ["appliedMemberList", postId],
+    queryFn: () => getAppliedMemberList(postId, { order: "DESC", page: 1, take: 100 }),
+  });
+  const {
+    data: memberListData,
+    error: memberListError,
+    isError: isMemberListError,
+  } = useQuery({
+    queryKey: ["memberList", postId],
+    queryFn: () => getTeamMember(postId, { order: "DESC", page: 1, take: 5 }),
+  });
 
-  const handleRejectMember = async (teamMemberId: number) => {
-    try {
-      await rejectMember(teamMemberId);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  if (isError) {
+    console.error(error);
+  }
 
-  const handleDeleteMember = async (teamMemberId: number) => {
-    try {
-      await deleteTeamMember(teamMemberId);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  if (isAppliedMemberListError) {
+    console.error(appliedMemberListError);
+  }
 
-  useEffect(() => {
-    const getDetailInfo = async () => {
-      const response = await getStudyManagement(postId);
-      try {
-        if (response && response.data) {
-          setDetailInfo(response.data);
-        } else {
-          window.alert(response.errorMessage);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getDetailInfo();
-  }, [postId]);
+  if (isMemberListError) {
+    console.error(memberListError);
+  }
 
   useEffect(() => {
-    const getAppliedMember = async () => {
-      const response = await getAppliedMemberList(postId, {
-        order: "DESC",
-        page: 1,
-        take: 100,
-      });
-      try {
-        if (response && response.data) {
-          setAppliedMemberList(response.data.data);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getAppliedMember();
-  }, [postId]);
+    if (detailInfoData) {
+      setDetailInfo(detailInfoData);
+    }
+  }, [detailInfoData]);
 
   useEffect(() => {
-    const getMember = async () => {
-      const response = await getTeamMember(postId, {
-        order: "DESC",
-        page: 1,
-        take: 5,
-      });
-      try {
-        if (response && response.data) {
-          setMemberList(response.data.data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getMember();
-  }, [postId]);
+    if (appliedMemberListData) {
+      setAppliedMemberList(appliedMemberListData.data);
+    }
+  }, [appliedMemberListData]);
+
+  useEffect(() => {
+    if (memberListData) {
+      setMemberList(memberListData.data);
+    }
+  }, [memberListData]);
 
   return (
     <S.Container>
       <S.Box>
         {detailInfo && <S.DetailSection detailInfo={detailInfo} />}
         <S.ListSection>
-          <AppliedList
-            detailInfo={appliedMemberList}
-            handleAcceptMember={(teamMemberId) => handleAcceptMember(teamMemberId)}
-            handleRejectMember={(teamMemberId) => handleRejectMember(teamMemberId)}
-          />
-          <MemberList detailInfo={memberList} handleOutMember={(teamMemberId) => handleDeleteMember(teamMemberId)} />
+          <AppliedList detailInfo={appliedMemberList} />
+          <MemberList detailInfo={memberList} />
         </S.ListSection>
         {detailInfo && <S.ButtonSection buttonType={detailInfo.status} isLeader={detailInfo.isLeader} />}
       </S.Box>
