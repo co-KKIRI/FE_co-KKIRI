c4cb8c300c45a266d30f314210227ace51343da6
김하은 <117327533+hankim0904@users.noreply.github.com>
2024-03-25 09:28:47 +0900

    Chore: CardCornerButton에 JSDoc 작성

--- co-kkiri/src/components/commons/CardCornerButton.tsx
+++ co-kkiri/src/components/commons/CardCornerButton.tsx
@@ -14,9 +14,17 @@ interface CardCornerButtonProps {
 }
 
 /**
+ * CardCornerButton 컴포넌트는 카드 모서리에 위치하는 버튼(스크랩, 관리, 리뷰 작성, 리뷰 보기)을 표시합니다.
  *
- * @property width - px단위
- * */
+ * @param {CardCornerButtonType} cardCornerType - 버튼의 타입을 결정합니다. ("scrap", "manage", "write", "view" 중 하나)
+ * 각 타입의 icon 이미지, width, text, onClick은 CARD_CORNER_BUTTON객체에서 관리합니다.
+ * @param {boolean} isScraped - 스크랩 버튼의 경우, 현재 스크랩이 되어있는지 여부를 나타냅니다.
+ * @param {number} [postId] - 관리, 리뷰 작성 버튼의 경우, 해당 포스트의 ID입니다.
+ *
+ * @example
+ * <CardCornerButton cardCornerType="scrap" isScraped={true} />
+ * <CardCornerButton cardCornerType="manage" postId={123} />
+ */
 export default function CardCornerButton({
   cardCornerType = "scrap",
   isScraped = false,
