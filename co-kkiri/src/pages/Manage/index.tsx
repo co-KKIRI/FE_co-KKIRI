d4f862d0d6cc51a0500136826423822d4c1966e5
Yoon-kyungLee <yk2588@nate.com>
2024-03-27 02:09:10 +0900

    Refector: manage page 리팩토링

--- co-kkiri/src/pages/Manage/index.tsx
+++ co-kkiri/src/pages/Manage/index.tsx
@@ -2,10 +2,8 @@ import MemberList from "@/components/domains/manage/MemberList";
 import * as S from "./styled";
 import AppliedList from "@/components/domains/manage/AppliedList";
 import { getAppliedMemberList, getStudyManagement } from "@/lib/api/post";
-import { useEffect, useState } from "react";
-import { AppliedMemberListApiResponseDto, StudyManagementApiResponseDto } from "@/lib/api/post/type";
+import { StudyManagementApiResponseDto } from "@/lib/api/post/type";
 import { getTeamMember } from "@/lib/api/teamMember";
-import { TeamMemberApiResponseDto } from "@/lib/api/teamMember/type";
 import { useQuery } from "@tanstack/react-query";
 
 interface ManageProps {
@@ -13,74 +11,38 @@ interface ManageProps {
 }
 
 export default function Manage({ postId }: ManageProps) {
-  const [detailInfo, setDetailInfo] = useState<StudyManagementApiResponseDto>();
-  const [appliedMemberList, setAppliedMemberList] = useState<
-    AppliedMemberListApiResponseDto["data"] | null | undefined
-  >();
-  const [memberList, setMemberList] = useState<TeamMemberApiResponseDto["data"] | null | undefined>();
-
-  const {
-    data: detailInfoData,
-    error,
-    isError,
-  } = useQuery({
+  const { data: detailInfo, error } = useQuery({
     queryKey: ["management", postId],
     queryFn: () => getStudyManagement(postId),
   });
-  const {
-    data: appliedMemberListData,
-    error: appliedMemberListError,
-    isError: isAppliedMemberListError,
-  } = useQuery({
+  const { data: appliedMemberList, error: appliedMemberListError } = useQuery({
     queryKey: ["appliedMemberList", postId],
     queryFn: () => getAppliedMemberList(postId, { order: "DESC", page: 1, take: 100 }),
   });
-  const {
-    data: memberListData,
-    error: memberListError,
-    isError: isMemberListError,
-  } = useQuery({
+  const { data: memberList, error: memberListError } = useQuery({
     queryKey: ["memberList", postId],
     queryFn: () => getTeamMember(postId, { order: "DESC", page: 1, take: 5 }),
   });
 
-  if (isError) {
+  if (error) {
     console.error(error);
   }
 
-  if (isAppliedMemberListError) {
+  if (appliedMemberListError) {
     console.error(appliedMemberListError);
   }
 
-  if (isMemberListError) {
+  if (memberListError) {
     console.error(memberListError);
   }
-
-  useEffect(() => {
-    if (detailInfoData) {
-      setDetailInfo(detailInfoData);
-    }
-  }, [detailInfoData]);
-
-  useEffect(() => {
-    if (appliedMemberListData) {
-      setAppliedMemberList(appliedMemberListData.data);
-    }
-  }, [appliedMemberListData]);
-
-  useEffect(() => {
-    if (memberListData) {
-      setMemberList(memberListData.data);
-    }
-  }, [memberListData]);
 
   return (
     <S.Container>
       <S.Box>
         {detailInfo && <S.DetailSection detailInfo={detailInfo} />}
         <S.ListSection>
-          <AppliedList detailInfo={appliedMemberList} />
-          <MemberList detailInfo={memberList} />
+          {appliedMemberList && <AppliedList detailInfo={appliedMemberList.data} />}
+          {memberList && <MemberList detailInfo={memberList.data} />}
         </S.ListSection>
         {detailInfo && <S.ButtonSection buttonType={detailInfo.status} isLeader={detailInfo.isLeader} />}
       </S.Box>
