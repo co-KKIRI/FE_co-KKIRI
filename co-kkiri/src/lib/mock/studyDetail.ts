54cfc1f6cc02b817dcc5bfec73d26d87e0cd2c0e
Joohyun Kim <trust987@naver.com>
2024-03-27 05:09:22 +0900

    Chore: StudyDetail mockdata 수정

--- co-kkiri/src/lib/mock/studyDetail.ts
+++ co-kkiri/src/lib/mock/studyDetail.ts
@@ -1,31 +1,8 @@
 import { CategoryList } from "@/types/categoryTypes";
+import { PostDetailApiResponseDto } from "@/lib/api/post/type";
 
-export interface DetailInfo {
-  postTitle: string; //제목
-  postContent: string; //내용
-  memberProfileImg: string; //작성자 프로필이미지
-  memberNickname: string; //작성자 닉네임
-  createdAt: string; //생성 시간 YYYY-MM-DD HH:MM:SS
-  views: number; //조회수
-  isScraped: boolean; //스크랩 되어있는지 아닌지
-  scraps: number; //스크랩수
-  type: CategoryList; //스터디 종류
-  recruitEndAt: string; // 모집 마감
-  progressPeriod: string; //진행 기간 => enum 타입으로 넣을 예정
-  progressWay: string; //진행 방법 (온라인/오프라인)
-  contactWay: string; //연락 방법
-  capacity: number; //모집 인원 ==> 테이블에 추가 예정
-  positions: string[]; //포지션
-  stacks: string[]; //스택
-  commentsNum: number;
-}
-
-interface StudyDetailData {
-  result: DetailInfo;
-}
-
-export const studyDetailData: StudyDetailData = {
-  result: {
+export const studyDetailData: PostDetailApiResponseDto = {
+  postDetails: {
     postTitle: "실제 사용할 쇼핑몰 웹프로젝트 만들어나가실 분 구합니다.", //제목
     postContent: `<section>
     <h1>모집 소개</h1>
@@ -53,20 +30,22 @@ export const studyDetailData: StudyDetailData = {
     <p>같이 열심히 해서 모두에게 좋은 포트폴리오 만들어봐요 :)</p>
     <p>많은 지원 부탁드립니다 :)</p>
 `, //내용
-    memberProfileImg: "", //작성자 프로필이미지
-    memberNickname: "코끼리", //작성자 닉네임
-    createdAt: "2024-03-19 13:00:00", //생성 시간 YYYY-M.-D.
-    views: 3, //조회수
-    isScraped: false, //스크랩 되어있는지 아닌지
-    scraps: 22, //스크랩수
+    userProfileImg: "", //작성자 프로필이미지
+    userNickname: "코끼리", //작성자 닉네임
+    createdAt: "2024-03-19 13:00:00", //생성 시간 YYYY-MM-DD HH:MM:SS
+    viewCount: 3, //조회수
+    scrapCount: 22, //스크랩수
     type: "PROJECT", //스터디 종류
     recruitEndAt: "2024-03-30", // 모집 마감
-    progressPeriod: "2개월", //진행 기간 => enum 타입으로 넣을 예정
+    progressPeriod: "2개월", //진행 기간
     progressWay: "온/오프라인", //진행 방법 (온라인/오프라인)
     contactWay: "오픈톡", //연락 방법
-    capacity: 2, //모집 인원 ==> 테이블에 추가 예정
+    capacity: 2, //모집 인원
     positions: ["프론트엔드", "안드로이드", "디자이너"], //포지션
     stacks: ["AWS", "React", "Node.js"], //스택
-    commentsNum: 6, //댓글 수
+    commentCount: 6, //댓글 수
+    link: "",
   },
+  isScraped: false, //스크랩 되어있는지 아닌지
+  postApplyStatus: "APPLIED",
 };
