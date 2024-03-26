970ef749bfdf56d2f47865e30cdfc4dfbd0a1d26
이나경 <jay3925@cau.ac.kr>
2024-03-27 02:01:39 +0900

    Feat: Dropdown 컴포넌트  error 관련 Prop 추가

--- co-kkiri/src/components/commons/DropDowns/Dropdown.tsx
+++ co-kkiri/src/components/commons/DropDowns/Dropdown.tsx
@@ -2,16 +2,27 @@ import useOpenToggle from "@/hooks/useOpenToggle";
 import SquareDropButton from "./commons/SquareDropButton";
 import DropMenu from "./commons/DropMenu";
 import styled from "styled-components";
+import DESIGN_TOKEN from "@/styles/tokens";
 
 interface DropdownProps {
   placeholder: string;
   selectedOption?: string | null;
   options: string[];
+  isError?: boolean;
+  helperText?: string;
   onSelect: (option: string) => void;
   dropdownRef?: React.RefCallback<HTMLButtonElement>;
 }
 
-export default function Dropdown({ placeholder, selectedOption, options, onSelect, dropdownRef }: DropdownProps) {
+export default function Dropdown({
+  placeholder,
+  selectedOption,
+  options,
+  onSelect,
+  isError,
+  helperText,
+  dropdownRef,
+}: DropdownProps) {
   const { isOpen, openToggle: toggleDropdown, ref } = useOpenToggle();
 
   const handleSelectOption = (option: string) => {
@@ -27,17 +38,30 @@ export default function Dropdown({ placeholder, selectedOption, options, onSelec
         $isSelected={!!selectedOption}
         $iconType="default"
         dropButtonRef={dropdownRef}
+        isError={isError}
       />
+      <HelperText>{helperText}</HelperText>
       <DropMenu isOpen={isOpen} handleSelectOption={handleSelectOption} $borderType="square" options={options} />
     </Container>
   );
 }
 
-const Container = styled.div`
+const { color, typography } = DESIGN_TOKEN;
+interface ContainerProps {
+  $isError?: boolean;
+}
+
+const Container = styled.div<ContainerProps>`
   width: 100%;
   display: flex;
   flex-direction: column;
   position: relative;
   padding: 0;
   gap: 0.8rem;
+`;
+
+const HelperText = styled.p`
+  text-align: start;
+  color: ${color.red};
+  ${typography.font12Medium}
 `;
