fbe9bf35c68bdf6faf72001201ad418781d57273
이나경 <jay3925@cau.ac.kr>
2024-03-27 02:01:25 +0900

    Feat: FormElement 컴포넌트 구현

---
+++ co-kkiri/src/components/commons/Form/FormElement.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import Label from "./Label";
+
+export interface InputProps {
+  onChange: (value: string) => void;
+  value: string;
+  isError?: boolean;
+  helperText: string;
+}
+
+interface FormElementProps {
+  label: string;
+  isEssential?: boolean;
+  InputComponent: React.ReactElement<InputProps>;
+}
+
+export default function FormElement({ label, isEssential, InputComponent }: FormElementProps) {
+  return (
+    <Container>
+      <Label label={label} isEssential={isEssential} />
+      {InputComponent}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
