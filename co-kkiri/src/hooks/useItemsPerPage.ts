2732b17bc1f12d835369256c68790b37308a70bb
Yoon-kyungLee <yk2588@nate.com>
2024-03-28 03:20:41 +0900

    Refector

--- co-kkiri/src/hooks/useItemsPerPage.ts
+++ co-kkiri/src/hooks/useItemsPerPage.ts
@@ -1,6 +1,26 @@
 import { useState, useEffect } from "react";
-import { getItemsPerPage } from "@/utils/getItemsPerPage";
 import useSideBarStore from "@/stores/sideBarStore";
+
+const getItemsPerPage = (isSideBarOpen: boolean) => {
+  const screenWidth = window.innerWidth;
+  let ITEMS_PER_PAGES;
+
+  if (screenWidth >= 1410) {
+    ITEMS_PER_PAGES = 16;
+  } else if (screenWidth >= 1200) {
+    if (isSideBarOpen) {
+      ITEMS_PER_PAGES = 12;
+    } else {
+      ITEMS_PER_PAGES = 16;
+    }
+  } else if (screenWidth >= 768) {
+    ITEMS_PER_PAGES = 8;
+  } else {
+    ITEMS_PER_PAGES = 3;
+  }
+
+  return ITEMS_PER_PAGES;
+};
 
 export function useItemsPerPage() {
   const isSideBarOpen = useSideBarStore((state) => state.isSideBarOpen);
