af98cb2f3ede5db06819c86a3bf8f912f575afb7
Joohyun Kim <trust987@naver.com>
2024-03-28 04:51:24 +0900

    Chore: comment 관련 타입명 수정

--- co-kkiri/src/lib/api/comment/index.ts
+++ co-kkiri/src/lib/api/comment/index.ts
@@ -1,22 +1,17 @@
 import { commentAddress } from "../address";
 import { apiRequest } from "../axios";
-import {
-  CommentApiRequestDto,
-  CommentApiResponseDto,
-  CreateCommentApiRequestDto,
-  ModifyCommentApiRequestDto,
-} from "./type";
+import { CommentApiRequestDto, CommentApiResponseDto, CommentPageMetaApiRequestDto } from "./type";
 
 /** 댓글 목록 가져오기 */
-export const getCommentList = (postId: number, qs: CommentApiRequestDto): Promise<CommentApiResponseDto> =>
+export const getCommentList = (postId: number, qs: CommentPageMetaApiRequestDto): Promise<CommentApiResponseDto> =>
   apiRequest("get", commentAddress.list(postId), null, qs);
 
 /** 댓글 달기 */
-export const createComment = (postId: number, data: CreateCommentApiRequestDto) =>
+export const createComment = (postId: number, data: CommentApiRequestDto) =>
   apiRequest("post", commentAddress.write(postId), data);
 
 /** 댓글 수정 */
-export const editComment = (postId: number, commentId: number, data: ModifyCommentApiRequestDto) =>
+export const editComment = (postId: number, commentId: number, data: CommentApiRequestDto) =>
   apiRequest("patch", commentAddress.commentId(postId, commentId), data);
 
 /** 댓글 삭제 */
--- co-kkiri/src/lib/api/comment/type.ts
+++ co-kkiri/src/lib/api/comment/type.ts
@@ -1,5 +1,5 @@
 // 스터디 상세 스터디 댓글 목록
-type CommentInfo = {
+export type CommentInfo = {
   commentId: number;
   commentMemberId: number;
   commentProfileImg: string;
@@ -18,7 +18,7 @@ type PageMeta = {
   hasNextPage: boolean; // 다음 페이지가 있는지
 };
 
-export type CommentApiRequestDto = {
+export type CommentPageMetaApiRequestDto = {
   order?: "ASC" | "DESC"; // 정렬 순서, ASC: 옛날순, DESC: 최신순
   page?: number; // 요청할 페이지
   take?: number; // 몇개 가져올지
@@ -26,12 +26,9 @@ export type CommentApiRequestDto = {
 
 // 댓글 목록
 export type CommentApiResponseDto = {
-  comments: CommentInfo[];
+  data: CommentInfo[];
   meta: PageMeta;
 };
 
 // 댓글 달기
-export type CreateCommentApiRequestDto = { memberId: number; content: string };
-
-//댓글 수정
-export type ModifyCommentApiRequestDto = { content: string };
+export type CommentApiRequestDto = { content: string };
